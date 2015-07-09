1c1d6c6408b543e85d524c951ae65d235fa59e44
msnatu <ms.natu@gmail.com>
2015-07-10 00:52:25 +0530

    Completing the voting module

--- server.go
+++ server.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"os"
 	"encoding/xml"
+	"strconv"
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -53,6 +54,7 @@ var page_templates = template.Must(template.ParseFiles(
 func main() {
 	http.Handle("/www/", http.StripPrefix("/www/", http.FileServer(http.Dir("www"))))
 	http.HandleFunc("/", loadHomePage)
+	http.HandleFunc("/vote", handleVote)
 	fmt.Printf("tested!!");
 	http.ListenAndServe(":8080", nil)
 }
@@ -94,14 +96,14 @@ func getPuppies(w http.ResponseWriter, p *HomePage) string {
 			for i := 0; i < len(photo_collection.Photo); i++ {
 				img_data := photo_collection.Photo[i]
 				img_url := "https://farm" + img_data.FarmId + ".staticflickr.com/" + img_data.ServerId + "/" + img_data.Id + "_" + img_data.Secret + "_q.jpg"
-
 				existing_votes := getImageVotes(img_data.Id)
-				fmt.Println(existing_votes)
 
-				img_elem += "<div class='puppy-image-container' up_vote='" + existing_votes[0] +"' down_vote='" + existing_votes[1] + "'>"
-				img_elem += "<img src='" + img_url + "' class='puppy-box' img_id='"+ img_data.Id +"'>"
+				img_elem += "<div class='puppy-image-container' img_id='"+ img_data.Id +"'>"
+				img_elem += "<img src='" + img_url + "' class='puppy-box'>"
 				img_elem += "<div class='puppy-image-upvote' style='display:none;'></div>"
+				img_elem += "<div class='puppy-image-upvote-info' style='display:none;'>" + existing_votes[0] +"</div>"
 				img_elem += "<div class='puppy-image-downvote' style='display:none;'></div>"
+				img_elem += "<div class='puppy-image-downvote-info' style='display:none;'>" + existing_votes[1] +"</div>"
 				img_elem += "</div>"
 			}
 		}
@@ -122,15 +124,62 @@ func getImageVotes(img_id string) []string {
 		up_vote string
 		down_vote string
 	)
+	//Select
 	rows, _ := db.Query("SELECT * FROM votes WHERE image_id = ?", img_id)
 	defer rows.Close()
+
+	total_rows := 0
 	for rows.Next() {
 		err = rows.Scan(&id, &image_id, &up_vote, &down_vote)
 		if err != nil {
 			fmt.Println(err)
 		}
-		fmt.Println(id, image_id)
+		total_rows++
+	}
+
+	//Insert
+	if total_rows == 0 {
+		stmt, _ := db.Prepare("INSERT INTO votes(image_id) VALUES(?)")
+		stmt.Exec(img_id)
 	}
 
 	return []string{up_vote, down_vote}
+}
+
+func handleVote(w http.ResponseWriter, r *http.Request) {
+	img_id := r.FormValue("img_id")
+	is_upvote := r.FormValue("is_upvote")
+
+	db, err := sql.Open("mysql", "root:p@ssw0rd@tcp(127.0.0.1:3306)/puppies")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
+
+	var (
+		up_vote string
+		down_vote string
+		resp_vote int
+	)
+
+	db.QueryRow("SELECT up_vote, down_vote FROM votes WHERE image_id = ?", img_id).Scan(&up_vote, &down_vote)
+	if(is_upvote == "true") {
+		var new_up_vote int
+		up_vote, _ := strconv.Atoi(up_vote)
+		new_up_vote = up_vote + 1
+		stmt, _ := db.Prepare("UPDATE votes SET up_vote = ? WHERE image_id = ?")
+		stmt.Exec(new_up_vote, img_id)
+		resp_vote = new_up_vote
+	} else {
+		var new_down_vote int
+		down_vote, _ := strconv.Atoi(down_vote)
+		new_down_vote = down_vote + 1
+		stmt, _ := db.Prepare("UPDATE votes SET down_vote = ? WHERE image_id = ?")
+		stmt.Exec(new_down_vote, img_id)
+		resp_vote = new_down_vote
+	}
+
+	type JsonResponse map[string]interface{}
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprint(w, resp_vote)
 }
--- www/js/puppies.js
+++ www/js/puppies.js
@@ -1,9 +1,56 @@
 $(document).ready(function() {
-    $('.puppy-image-container').hover(
-    function () {
-        $(this).find('.puppy-image-upvote, .puppy-image-downvote').fadeIn(200);
-    },
-    function () {
-        $(this).find('.puppy-image-upvote, .puppy-image-downvote').fadeOut(500);
-    });
+
+    var ImageHandler = function() {
+        this.init();
+    };
+
+    ImageHandler.prototype = {
+        init: function () {
+            this.showVotes();
+            this.bindVotingEvents();
+        },
+
+        showVotes: function() {
+            $('.puppy-image-container').hover(
+            function () {
+                $(this).find('.puppy-image-upvote, .puppy-image-downvote, .puppy-image-upvote-info, .puppy-image-downvote-info').fadeIn(200);
+            },
+            function () {
+                $(this).find('.puppy-image-upvote, .puppy-image-downvote, .puppy-image-upvote-info, .puppy-image-downvote-info').fadeOut(500);
+            });
+        },
+
+        bindVotingEvents: function() {
+            var self = this;
+            $('.puppy-image-upvote').unbind('click').bind('click', function() {
+                var img_id = $(this).parent().attr("img_id");
+                self.votePuppy(img_id, true);
+            });
+            $('.puppy-image-downvote').unbind('click').bind('click', function() {
+                var img_id = $(this).parent().attr("img_id");
+                self.votePuppy(img_id, false);
+            });
+        },
+
+        votePuppy: function (img_id, is_upvote) {
+            var self = this;
+            $.ajax({
+                url: 'vote',
+                data: {
+                    img_id: img_id,
+                    is_upvote: is_upvote
+                },
+                type: 'POST',
+                success: function(new_vote) {
+                    if(is_upvote) {
+                        $('.puppy-image-container[img_id="' + img_id + '"]').find('.puppy-image-upvote-info').html(new_vote);
+                    } else {
+                        $('.puppy-image-container[img_id="' + img_id + '"]').find('.puppy-image-downvote-info').html(new_vote);
+                    }
+                }
+            });
+        }
+    };
+
+    new ImageHandler();
 });
